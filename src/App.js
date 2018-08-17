5474b902f7d35dfe42e995b88eb24e684308e95d
Elena Dontsova <for.dontsova.elena@gmail.com>
2018-08-17 19:40:07 +0300

    feat: google maps errors handling and move data to separate json file

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@ import Map from './components/Map.js'
 import Filter from './components/Filter.js'
 import List from './components/List.js'
 import Info from './components/Info.js'
+import * as defaultLocations from './data/mountains.json'
 
 class App extends Component {
 
-  defaultLocations = [
-    {title: 'Everest', coords: { lat: 27.988056, lng: 86.925278 }},
-    {title: 'Kanchenjunga', coords: { lat: 27.703333, lng: 88.1475 }},
-    {title: 'Lhotse', coords: { lat: 27.961667, lng: 86.933056 }},
-    {title: 'Makalu', coords: { lat: 27.889722, lng: 87.088889 }},
-    {title: 'Cho Oyu', coords: { lat: 28.094167, lng: 86.660833 }},
-    {title: 'Dhaulagiri I', coords: { lat: 28.696667, lng: 83.493056 }},
-    {title: 'Manaslu', coords: { lat: 28.55, lng: 84.559722 }},
-    {title: 'Annapurna I', coords: { lat: 28.595556, lng: 83.820278 }},
-    {title: 'Shishapangma', coords: { lat: 28.353333, lng: 85.778611 }},
-    {title: 'Ama Dablam', coords: { lat: 27.861111, lng: 86.861111 }},
-    {title: 'Machapuchare', coords: { lat: 28.495, lng: 83.949167 }},
-    {title: 'Langtang Lirung', coords: { lat: 28.256111, lng: 85.516944 }}
-  ]
-
   state = {
     inputString: '',
-    locations: this.defaultLocations,
+    locations: defaultLocations,
     active: ''
   }
 
@@ -35,9 +21,9 @@ class App extends Component {
     const str = value.trim().toLowerCase();
     let newLocations;
     if (str === '') {
-      newLocations = this.defaultLocations;
+      newLocations = defaultLocations;
     } else {
-      newLocations = this.defaultLocations.filter(location => {
+      newLocations = defaultLocations.filter(location => {
         const mountain = location.title.toLowerCase();
         return mountain.indexOf(str) !== -1;
       });
@@ -51,6 +37,26 @@ class App extends Component {
   }
 
   render() {
+
+    // display google map or error message depends on script loading
+    let mapArea;
+    if (window.google) {
+      mapArea = (<Map
+          onMarkerClick={this.setActive}
+          active={this.state.active}
+          locations={this.state.locations}
+          onInfoClose={() => this.setState({infoWindowShown: 'none'})}
+          containerElement={
+            <div className="map-container"
+            style={{ height: `300px` }}
+            role="application"
+            aria-label="Map with markers for Himalayan mountains" />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />);
+      } else {
+        mapArea = (<h1>{ "Map isn't loaded" }</h1>);
+      }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -64,20 +70,7 @@ class App extends Component {
           <a href="#filter" className="skip-link">Skip map elements and go to the main content</a>
         </header>
         <main className="App-main">
-          <Map
-            onMarkerClick={this.setActive}
-            active={this.state.active}
-            locations={this.state.locations}
-            onInfoClose={() => this.setState({infoWindowShown: 'none'})}
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDQlk_GImQ5uy8SzK0ku3IoPGZpSlFXTK4&v=3"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={
-              <div className="map-container"
-              style={{ height: `300px` }}
-              role="application"
-              aria-label="Map with markers for Himalayan mountains" />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
+          { mapArea }
           <div className="App-results">
             <List
               locations={ this.state.locations }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,42 +1,43 @@
 import React from 'react'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import './../style/App.css'
 import './../style/media.css'
 import * as everest from './../icons/goal.svg'
 
 // Map component built with react-google-maps
-const Map = withScriptjs(withGoogleMap((props) => {
-  return(
-    <GoogleMap
-        mapTypeId='terrain'
-        ref={(map) => {
-          if (map && props.locations.length > 3) {
-            const bounds = new window.google.maps.LatLngBounds();
-            props.locations.forEach(location => {
-              const latLng = new window.google.maps.LatLng(location.coords);
-              bounds.extend(latLng);
-            });
-            map.fitBounds(bounds, 10);
-          }
-        }}>
+
+const Map = withGoogleMap((props) => {
+    return(
+      <GoogleMap
+      mapTypeId='terrain'
+      ref={(map) => {
+        if (map && props.locations.length > 3) {
+          const bounds = new window.google.maps.LatLngBounds();
+          props.locations.forEach(location => {
+            const latLng = new window.google.maps.LatLng(location.coords);
+            bounds.extend(latLng);
+          });
+          map.fitBounds(bounds, 10);
+        }
+      }}>
       {props.locations.map(location =>
         <Marker
-            key={ location.title }
-            position={ location.coords }
-            title={ location.title }
-            icon={ location.title === 'Everest' ? everest : null }
-            animation={ props.active === location.title ? window.google.maps.Animation.BOUNCE : null}
-            onClick={ () => { props.onMarkerClick(location.title) } }
-            >
+        key={ location.title }
+        position={ location.coords }
+        title={ location.title }
+        icon={ location.title === 'Everest' ? everest : null }
+        animation={ props.active === location.title ? window.google.maps.Animation.BOUNCE : null}
+        onClick={ () => { props.onMarkerClick(location.title) } }
+        >
         {props.active === location.title &&
           (<InfoWindow onCloseClick={ props.onInfoClose }>
             <p>{ location.title }</p>
-          </InfoWindow>)
-        }
-        </Marker>
+            </InfoWindow>)
+          }
+          </Marker>
         )}
-    </GoogleMap>
-  )
-}))
+        </GoogleMap>
+      )
+    })
 
 export default Map
